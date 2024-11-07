28607f6cec0db7968c5f4f0f988153932a1ecca9
ZephyrTFA <matthew@tfaluc.com>
2024-11-06 20:53:55 -0500

    change to rust helper

--- package.nix
+++ package.nix
@@ -1,36 +1,17 @@
 { pkgs, stdenv, config, lib, fetchFromGitHub, ... }:
 
-let
-version = "main";
-
-in stdenv.mkDerivation {
-    name = "dragon-bot";
-
-    meta = with lib; {
-      platforms = platforms.x86_64;
-    };
-
+rustPlatform.buildRustPackage rec {
+    pname = "dragon-bot";
+    version = "0.1.0";
     src = fetchFromGitHub {
         owner = "tgstation";
         repo = "dragon-bot";
-        rev = "${version}";
-        hash = "sha256-xnI1v7jW7waIGQvv2iTeGHKRTDGK+5SWS/DH1mzk+xk=";
+        rev = "main";
+        sha256 = "sha256-xnI1v7jW7waIGQvv2iTeGHKRTDGK+5SWS/DH1mzk+xk=";
     };
-
-    buildInputs = with pkgs; [
-        git cargo
-    ];
-
-    buildPhase = ''
-        cargo build --release
-    '';
-
-    testPhase = ''
-        cargo test --release
-    '';
-
-    installPhase = ''
-        mkdir -p $out/bin
-        mv ./target/release/dragon-bot $out/bin/dragon-bot
-    '';
+    cargoHash = lib.fakeHash;
+    meta = {
+        description = "A Discord bot for the /tg/station Discord server.";
+        license = lib.licenses.mit;
+    }
 }
